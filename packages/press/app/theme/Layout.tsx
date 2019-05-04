f13226bc2d6a2bdb1d72a2550e5ffe0c187f3a75
Spades-S <allengefeng@163.com>
2019-05-04 18:27:36 +0800

    feat(press): add highlight theme

--- packages/press/app/theme/Layout.tsx
+++ packages/press/app/theme/Layout.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
+import './styles/highlight.css';
 
 export default class Layout extends React.Component<any> {
   public render() {
